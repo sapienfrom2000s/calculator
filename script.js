3ab1a5de9267f65c7e29beb4f22372e57100a033
brickwall <sk292207@gmail.com>
2022-09-28 00:38:20 +0530

    Activate digit buttons, operation buttons and equals button
    Combine above three to simulate basic calculation on two op-
    erands

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /*JS uses IEEE 754 64-bit to store primitive number data type*/
 
 
+let calculator = {num1:'', num2:'', expression:'', operation:'',togglekey:'num1',result:''};
+
+
 function add(a, b){
     return a+b;
 }
@@ -17,23 +20,23 @@ function multiply(a, b){
     return a*b;
 }
 
-function operate(operation, a, b){
-    let result;
+function operate(operation,a, b){
+    // let result;
     switch(operation){      
         case "+": 
-            result = add(a, b); //The reason result is kept local beacause I don't want it to be influenced by othercases howsoever.
+            return add(a, b); //The reason result is kept local beacause I don't want it to be influenced by othercases howsoever.
             break;
         
         case "-":
-            result = subtract(a, b);
+            return subtract(a, b);
             break;
 
         case "/":
-            result = divide(a, b);
+            return divide(a, b);
             break;
 
         case "*":
-            result = multiply(a, b);
+            return multiply(a, b);
             break;
 
         default:
@@ -42,5 +45,56 @@ function operate(operation, a, b){
     }
     console.log(`${result}`);
 
-
 }
+
+function numberButtonListener(){
+    
+    const grabNumberButtons = document.querySelectorAll(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero"); 
+    grabNumberButtons.forEach( button => button.addEventListener("click",numberInput));   //works for both num1 and num2
+}
+
+function numberInput(e){
+    calculator[calculator.togglekey] += e.target.innerHTML;
+    console.log(calculator.num1, calculator.num2.length);
+    updateDisplay(e);
+    if(calculator.num1.length>0){
+        operationListener();      //Operation will only be available when there is some value of num1
+    }
+    if(calculator.num2.length>0){
+        activateEqualsbutton();   //so that equals only works when you have two valid variables and a operation
+    }
+}
+
+function activateEqualsbutton(){
+    const grabEqualsbutton = document.querySelector(".equals");
+    grabEqualsbutton.addEventListener("click",callforResult);
+}
+
+function callforResult(e){
+    calculator.result = operate(calculator.operation, Number(calculator.num1),Number(calculator.num2));
+    const selectDisplaydiv = document.querySelector(".display");
+    selectDisplaydiv.innerHTML = calculator.result;
+    calculator.num1 = calculator.result.toString();
+}
+
+function operationListener(){
+    let grabOperationButtons = document.querySelectorAll(".plus, .minus, .multiply, .divide");
+    grabOperationButtons.forEach( button => button.addEventListener("click",operation));
+}
+
+function operation(e){
+    calculator.operation = e.target.innerHTML;
+    let grabOperationButtons = document.querySelectorAll(".plus, .minus, .multiply, .divide");
+    grabOperationButtons.forEach(button => button.removeEventListener("click",operation));   //as only one operation is required
+    calculator.togglekey = "num2";
+    updateDisplay(e);
+}
+
+function updateDisplay(e){
+    const selectDisplaydiv = document.querySelector(".display");
+    calculator.expression += e.target.innerHTML;
+    selectDisplaydiv.innerHTML = calculator.expression;
+}
+
+
+numberButtonListener();
